67728917a0f16ae9ff84882c9d19966b2f0424f5
Shamsunnahar Afifa <shamsunnaharafifa@gmail.com>
2019-11-11 19:52:05 +0600

    radio button added in order activity

--- AndroidDroidCafeApp/app/src/main/java/com/example/afifa123/androiddroidcafeapp/OrderActivity.kt
+++ AndroidDroidCafeApp/app/src/main/java/com/example/afifa123/androiddroidcafeapp/OrderActivity.kt
@@ -2,7 +2,10 @@ package com.example.afifa123.androiddroidcafeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import android.widget.RadioButton
 
 class OrderActivity : AppCompatActivity() {
 
@@ -17,4 +20,17 @@ class OrderActivity : AppCompatActivity() {
         textView.text = message
 
     }
+    private fun displayToast(message: String){
+        Toast.makeText(applicationContext,message, Toast.LENGTH_SHORT).show()
+    }
+
+    fun onRadioButtonClicked(view: View) {
+        val checked = (view as RadioButton).isChecked
+        when (view.getId()) {
+           R.id.same_day -> displayToast(getString(R.string.same_day_messenger_service_text))
+            R.id.next_day -> displayToast(getString(R.string.next_messenger_service_text))
+            R.id.pick_up -> displayToast(getString(R.string.pick_up_service_text))
+        }
+    }
 }
+
