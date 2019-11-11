7edab2eda086b56edd0adaea5628adfddbc6ac97
Shamsunnahar Afifa <shamsunnaharafifa@gmail.com>
2019-11-11 15:35:24 +0600

    init order activity

--- AndroidDroidCafeApp/app/src/main/java/com/example/afifa123/androiddroidcafeapp/OrderActivity.kt
+++ AndroidDroidCafeApp/app/src/main/java/com/example/afifa123/androiddroidcafeapp/OrderActivity.kt
@@ -2,11 +2,18 @@ package com.example.afifa123.androiddroidcafeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.widget.TextView
 
 class OrderActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order)
+
+        intent = getIntent()
+        var message = "Order: "+intent.getStringExtra(EXTRA_MESSAGE)
+        var textView: TextView = findViewById(R.id.order_text_view)
+        textView.setText(message)
     }
 }
